61db4cb6156d8285a356c2aaa487364377c341ca
김평열 <yeolpyeong@gmail.com>
2023-09-23 15:01:49 +0900

    Time: 56 ms (27.91%), Space: 41 MB (98.17%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(anotherVal) {
+            if (val === anotherVal) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(anotherVal) {
+            if (val !== anotherVal) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
